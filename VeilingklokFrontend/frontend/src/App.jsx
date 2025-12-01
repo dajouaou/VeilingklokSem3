668d7c0c7df4dde35fccb4a02da2ed42ac68e9a1
dajouaou <24146145@student.hhs.nl>
2025-12-01 16:14:20 +0100

    path fix

--- VeilingklokFrontend/frontend/src/App.jsx
+++ VeilingklokFrontend/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import AppRouter from "./router/AppRouter";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles.css";
+import "frontend/src/styles/styles.css";
 
 export default function App() {
     return <AppRouter />;
