f23a6b47e96af01d278607a8fcfa92c8507076da
Sofia-bit-2025 <24121681@student.hhs.nl>
2025-12-05 19:16:55 +0100

    debug

--- Veilingklok/Features/Auth/Services/AuthService.cs
+++ Veilingklok/Features/Auth/Services/AuthService.cs
@@ -40,7 +40,7 @@ public class AuthService
             Email = email,
             Voornaam = voornaam,
             Achternaam = achternaam,
-            Rol = rol,                    // gebruik de enum
+            Role = rol,                    // gebruik de enum
             PasswordHash = hashed,
             CreatedAtUtc = DateTime.UtcNow
         };
