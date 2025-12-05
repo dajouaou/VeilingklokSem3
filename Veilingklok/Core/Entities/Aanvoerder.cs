036bb19e4fe4b2224d2ceff6a4c1d1c6131029dd
Qahqaei <24121681@student.hhs.nl>
2025-12-05 19:46:59 +0100

    Update Aanvoerder.cs

--- Veilingklok/Core/Entities/Aanvoerder.cs
+++ Veilingklok/Core/Entities/Aanvoerder.cs
@@ -2,12 +2,14 @@ namespace Veilingklok.Core.Entities;
 
 public class Aanvoerder
 {
-    public int Id { get; set; }                 // PK 
-    public int GebruikerId { get; set; }        // FK
-    public string Naam { get; set; } = "";
+    public int Id { get; set; }
 
+    public int GebruikerId { get; set; }
     public Gebruiker? Gebruiker { get; set; }
+
+    public string Naam { get; set; } = string.Empty;
+    public string? ContactInfo { get; set; }
 
     public List<Product> Producten { get; set; } = new();
     public List<VeilingProduct> VeilingProducten { get; set; } = new();
-} 
+}
