8099d394c114e88844b5e1602190cc7f3e431c59
Qahqaei <24121681@student.hhs.nl>
2025-12-05 19:46:37 +0100

    Update AuditEntry.cs

--- Veilingklok/Core/Entities/AuditEntry.cs
+++ Veilingklok/Core/Entities/AuditEntry.cs
@@ -2,15 +2,16 @@ namespace Veilingklok.Core.Entities;
 
 public class AuditEntry
 {
-    public int Id { get; set; }                 // PK
-    public int VeilingId { get; set; }          // FK
-    public int ActorGebruikerId { get; set; }   // FK
+    public int Id { get; set; }
 
-    public string Action { get; set; } = "";    //  "AUCTION_START", "BID_PLACED"
+    public int VeilingId { get; set; }
+    public Veiling? Veiling { get; set; }
+
+    public int ActorGebruikerId { get; set; }
+    public Gebruiker? ActorGebruiker { get; set; }
+
+    public string Action { get; set; } = string.Empty;
     public string? Details { get; set; }
 
     public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
-
-    public Veiling? Veiling { get; set; }
-    public Gebruiker? ActorGebruiker { get; set; }
-} 
+}
