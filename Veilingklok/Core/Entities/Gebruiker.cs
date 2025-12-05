86b28e0ab44a50dc4e48ff1ffc6175ed0dcbfe0a
Sofia-bit-2025 <24121681@student.hhs.nl>
2025-12-05 19:09:36 +0100

    rolveld in gebruiker aangepast en validaties toegevoegd

--- Veilingklok/Core/Entities/Gebruiker.cs
+++ Veilingklok/Core/Entities/Gebruiker.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Veilingklok.Core.Enums;
 
 namespace Veilingklok.Core.Entities;
@@ -6,19 +7,30 @@ public class Gebruiker
 {
     public int Id { get; set; }
 
+    [Required, MaxLength(64)]
     public string Username { get; set; } = string.Empty;
+
+    [Required, EmailAddress, MaxLength(256)]
     public string Email { get; set; } = string.Empty;
+
+    [Required, MaxLength(255)]
     public string PasswordHash { get; set; } = string.Empty;
 
-    public string? Naam { get; set; }
+    [Required, MaxLength(64)]
     public string Voornaam { get; set; } = string.Empty;
+
+    [Required, MaxLength(64)]
     public string Achternaam { get; set; } = string.Empty;
 
     public UserRole Role { get; set; } = UserRole.Koper;
 
     public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
 
+
     public Aanvoerder? Aanvoerder { get; set; }
-    public Koper? Koper { get;  set; }
+    public Koper? Koper { get; set; }
     public VM? VM { get; set; }
+
+    
+    public string FullName => $"{Voornaam} {Achternaam}".Trim();
 }
