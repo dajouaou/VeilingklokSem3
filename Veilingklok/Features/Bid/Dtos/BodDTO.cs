1584d039601221b4d57a661263c2acc3c6b37d94
Laibakhan123 <24054992@student.hhs.nl>
2025-12-06 12:29:36 +0100

    bod dto verandering en dd

---
+++ Veilingklok/Features/Bid/Dtos/BodDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Veilingklok.Features.Bod.Dtos
+{
+    public class BodDTO
+    {
+        public int Id { get; set; }
+        public decimal Bedrag { get; set; }
+        public DateTime Tijdstip { get; set; }
+        public string KoperUsername { get; set; } = string.Empty;
+        public int VeilingId { get; set; }
+    }
+}
