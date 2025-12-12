018c5492b321d33dcaab1c4d7fabe32b4035f1b3
dajouaou <24146145@student.hhs.nl>
2025-12-12 14:39:36 +0100

    xunit unittestproject (mstest naar xunit) en aantal dependencies toegevoegd

--- VeilingklokUnitTest/UnitTest1.cs
+++ VeilingklokUnitTest/UnitTest1.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-
 namespace VeilingklokUnitTest
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void Test1()
         {
+
         }
     }
 }
