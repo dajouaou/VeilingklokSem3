bc11e6f1baf5a3838ff4d409433bf2e35cc596e3
Laibakhan123 <24054992@student.hhs.nl>
2026-01-14 00:14:58 +0100

    aanvoerder cs verandering

--- Veilingklok/Core/Entities/Aanvoerder.cs
+++ Veilingklok/Core/Entities/Aanvoerder.cs
@@ -2,11 +2,10 @@ namespace Veilingklok.Core.Entities;
 
 public class Aanvoerder
 {
-    public int Id { get; set; }                 // PK 
-    public int GebruikerId { get; set; }        // FK
-    public string Naam { get; set; } = "";
+    public int Id { get; set; }                 // Primaire sleutel
+    public int GebruikerId { get; set; }        // Foreign key naar Gebruiker
+    public string Naam { get; set; } = "";      // Weergavenaam van de aanvoerder
 
-    public Gebruiker? Gebruiker { get; set; }
-    public List<Aanmelding> Aanmeldingen { get; set; } = new();
-
+    public Gebruiker? Gebruiker { get; set; }   // Navigatie naar gekoppelde gebruiker (kan null zijn)
+    public List<Aanmelding> Aanmeldingen { get; set; } = new(); // Alle aanmeldingen van deze aanvoerder
 }
