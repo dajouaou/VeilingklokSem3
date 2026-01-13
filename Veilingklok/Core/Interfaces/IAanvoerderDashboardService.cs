77f7af156f280943d351dc9db3998ffb2c84565f
Laibakhan123 <24054992@student.hhs.nl>
2026-01-14 00:15:30 +0100

    services aanpassen

--- Veilingklok/Core/Interfaces/IAanvoerderDashboardService.cs
+++ Veilingklok/Core/Interfaces/IAanvoerderDashboardService.cs
@@ -4,10 +4,19 @@ namespace Veilingklok.Core.Interfaces
 {
     public interface IAanvoerderDashboardService
     {
+        // Maakt een nieuwe aanmelding aan voor de ingelogde aanvoerder
         Task<AanmeldingListItemDto> CreateAanmeldingAsync(int gebruikerId, AanmeldingCreateDto dto, string? fotoUrl);
+
+        // Past een bestaande aanmelding aan (alleen van de ingelogde aanvoerder)
         Task<AanmeldingListItemDto> UpdateAanmeldingAsync(int gebruikerId, int id, AanmeldingUpdateDto dto, string? fotoUrl);
+
+        // Verwijdert een aanmelding (alleen van de ingelogde aanvoerder)
         Task DeleteAanmeldingAsync(int gebruikerId, int id);
+
+        // Haalt aanmeldingen op, eventueel gefilterd op veildatum
         Task<IReadOnlyList<AanmeldingListItemDto>> GetAanmeldingenAsync(int gebruikerId, DateTime? veildatum);
+
+        // Haalt dashboard statistieken op, eventueel gefilterd op veildatum
         Task<AanvoerderStatsDto> GetStatsAsync(int gebruikerId, DateTime? veildatum);
     }
 }
