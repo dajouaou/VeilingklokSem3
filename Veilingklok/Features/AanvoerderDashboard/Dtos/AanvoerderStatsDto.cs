ab3e55d06c0077b277f6761d9ea0e470093c00aa
Laibakhan123 <24054992@student.hhs.nl>
2026-01-14 00:21:12 +0100

    opbergst gefixt

--- Veilingklok/Features/AanvoerderDashboard/Dtos/AanvoerderStatsDto.cs
+++ Veilingklok/Features/AanvoerderDashboard/Dtos/AanvoerderStatsDto.cs
@@ -2,8 +2,12 @@
 {
     public class AanvoerderStatsDto
     {
+
         public int TotaalAantalAanmeldingen { get; set; }
+
         public int AantalVerkocht { get; set; }
+
+        // Totale opbrengst van verkochte items
         public decimal TotaleOpbrengst { get; set; }
     }
 }
