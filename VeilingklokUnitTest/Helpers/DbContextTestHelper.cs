fdf35e6fea7f9c90cfe4d66dc990736b81c1a702
sbansie <24055034@student.hhs.nl>
2026-01-14 00:20:12 +0100

    DbContextTestHelper aangemaakt

---
+++ VeilingklokUnitTest/Helpers/DbContextTestHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Veilingklok.Infrastructure.Database;
+
+namespace VeilingklokUnitTest.Helpers;
+
+public static class DbContextTestHelper
+{
+    public static MyContext Create(string? name = null)
+    {
+        var options = new DbContextOptionsBuilder<MyContext>()
+            .UseInMemoryDatabase(name ?? Guid.NewGuid().ToString())
+            .EnableSensitiveDataLogging()
+            .Options;
+
+        var db = new MyContext(options);
+        db.Database.EnsureCreated();
+        return db;
+    }
+}
