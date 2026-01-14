ec7bb796bb0272b31cd75cae89d413ba13afd0f6
Qahqaei <24121681@student.hhs.nl>
2026-01-14 07:58:16 +0100

    Update VeilingController.cs

--- Veilingklok/Features/Veiling/Controllers/VeilingController.cs
+++ Veilingklok/Features/Veiling/Controllers/VeilingController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Veilingklok.Core.Interfaces;
 using Veilingklok.Features.Veiling.Dtos;
@@ -8,16 +8,19 @@ namespace Veilingklok.Features.Veiling.Controllers
     [ApiController]
     [Route("api/veiling")]
     [Authorize(Roles = "Veilingmeester")]
+    // Controller voor acties van de veilingmeester
     public class VeilingController : ControllerBase
     {
         private readonly IVeilingService _service;
 
+        // Injecteert de veilingservice
         public VeilingController(IVeilingService service)
         {
             _service = service;
         }
 
         [HttpPost("start")]
+        // Start een nieuwe veiling
         public async Task<IActionResult> Start([FromBody] StartVeilingDto dto)
         {
             var overzicht = await _service.StartVeilingAsync(dto.Veildatum, dto.LeverDatum, dto.StartTijd);
@@ -26,12 +29,14 @@ namespace Veilingklok.Features.Veiling.Controllers
 
 
         [HttpGet("{id}")]
+        // Haalt details van een veiling op
         public async Task<IActionResult> GetDetails(int id)
         {
             return Ok(await _service.GetDetailsAsync(id));
         }
 
         [HttpPost("{id}/pause")]
+        // Pauzeert een lopende veiling
         public async Task<IActionResult> Pause(int id)
         {
             await _service.PauseAsync(id);
@@ -39,6 +44,7 @@ namespace Veilingklok.Features.Veiling.Controllers
         }
 
         [HttpPost("{id}/resume")]
+        // Hervat een gepauzeerde veiling
         public async Task<IActionResult> Resume(int id)
         {
             await _service.ResumeAsync(id);
@@ -46,6 +52,7 @@ namespace Veilingklok.Features.Veiling.Controllers
         }
 
         [HttpPost("{id}/stop")]
+        // Stopt en sluit een veiling af
         public async Task<IActionResult> Stop(int id)
         {
             await _service.StopAsync(id);
@@ -53,6 +60,7 @@ namespace Veilingklok.Features.Veiling.Controllers
         }
 
         [HttpGet("dagen")]
+        // Haalt alle veildagen op
         public async Task<ActionResult<List<string>>> GetVeilingDagen()
         {
             var dagen = await _service.GetVeilingDagenAsync();
