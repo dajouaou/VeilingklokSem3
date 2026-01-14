e546d612782a47eaca30cb82f84123a4be4666bd
Qahqaei <24121681@student.hhs.nl>
2026-01-14 08:10:20 +0100

    Update VeilingmeesterConfiguration.cs

--- Veilingklok/Infrastructure/Database/Configurations/VeilingmeesterConfiguration.cs
+++ Veilingklok/Infrastructure/Database/Configurations/VeilingmeesterConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Veilingklok.Core.Entities;
 
@@ -6,19 +6,25 @@ namespace Veilingklok.Infrastructure.Database.Configurations
 {
     public class VeilingmeesterConfiguration : IEntityTypeConfiguration<Veilingmeester>
     {
+        // Configuratie voor hoe de Veilingmeester-entity in de database wordt opgeslagen
         public void Configure(EntityTypeBuilder<Veilingmeester> builder)
         {
+            // Zet de tabelnaam
             builder.ToTable("Veilingmeesters");
 
+            // Stelt de primary key in
             builder.HasKey(x => x.Id);
 
+            // Regels voor de Naam-kolom
             builder.Property(x => x.Naam)
                 .HasMaxLength(128)
                 .IsRequired();
 
+            // Zorgt dat elke gebruiker maar één veilingmeester-profiel kan hebben
             builder.HasIndex(x => x.GebruikerId)
                 .IsUnique();
 
+            // Configureert de 1-op-1 relatie met Gebruiker
             builder.HasOne(x => x.Gebruiker)
                 .WithOne(g => g.Veilingmeester)
                 .HasForeignKey<Veilingmeester>(x => x.GebruikerId)
