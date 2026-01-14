8557259ffe7157cb37ea1fd0f7152d49727564e7
sbansie <24055034@student.hhs.nl>
2026-01-14 18:30:30 +0100

    WachtrijItemDto geüpdatet

--- Veilingklok/Features/Veiling/Dtos/WachtrijItemDto.cs
+++ Veilingklok/Features/Veiling/Dtos/WachtrijItemDto.cs
@@ -1,18 +1,33 @@
 ﻿namespace Veilingklok.Features.Veiling.Dtos
 {
+    // DTO voor een product dat in de wachtrij van de veiling staat
     public class WachtrijItemDto
     {
         public int VeilingProductId { get; set; }
+        // ID van het veilingproduct
+
         public int Volgorde { get; set; }
+        // Positie van het product in de veilingvolgorde
 
         public string Soort { get; set; } = "";
+        // Soort product
+
         public string? FotoUrl { get; set; }
+        // Optionele foto van het product
 
         public decimal MaximumPrijs { get; set; }
+        // Startprijs van dit product
+
         public decimal MinimumPrijs { get; set; }
+        // Minimumprijs waar de klok kan stoppen
 
         public int ResterendeHoeveelheid { get; set; }
+        // Aantal eenheden dat nog beschikbaar is
+
         public int AanvoerderId { get; set; }
+        // ID van de aanvoerder
+
         public string AanvoerderNaam { get; set; } = "";
-}
+        // Naam van de aanvoerder
+    }
 }
