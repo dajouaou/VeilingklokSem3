6898ca8f075b4e4853db514a3ffe440452e69f49
sbansie <24055034@student.hhs.nl>
2026-01-14 19:42:54 +0100

    VeilingPublicController_GetActief_NoVeiling_Tests.cs aangemaakt

---
+++ VeilingklokUnitTest/Veiling/VeilingPublicController_GetActief_NoVeiling_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeilingklokUnitTest.Veiling
+{
+    internal class VeilingPublicController_GetActief_NoVeiling_Tests
+    {
+    }
+}
