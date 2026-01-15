695234ccf9c4dca81eb325a947cea0c02faa3c6a
dajouaou <24146145@student.hhs.nl>
2026-01-15 13:24:32 +0100

    ;

--- Veilingklok/Program.cs
+++ Veilingklok/Program.cs
@@ -124,7 +124,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(allowedOrigins)
               .AllowAnyHeader()
-              .AllowAnyMethod()
+              .AllowAnyMethod();
     });
 });
 
