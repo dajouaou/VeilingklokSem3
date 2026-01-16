a7e6e78c2f99c5f21ff91e886a6c5db31c828ea5
dajouaou <24146145@student.hhs.nl>
2026-01-16 11:01:31 +0100

    path veranderd van apibaseurl

--- frontendveilingklok/src/shared/icons/iconUrl.js
+++ frontendveilingklok/src/shared/icons/iconUrl.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from "../../../config/apiBaseUrl";
+import { API_BASE_URL } from "../../config/apiBaseUrl";
 
 export function iconUrl(path) {
     return `${API_BASE_URL}/icons/${path}`;
